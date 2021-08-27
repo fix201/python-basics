802ae39b2e40e0e208e62292877b4b07507b3d90
Olufisayo Joseph Ayodele <fisayoayodele01@gmail.com>
2021-08-26 23:34:25 -0500

    added to list comprehensions

--- variables.py
+++ variables.py
@@ -40,6 +40,9 @@ thislist.remove("apple")
 fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 newlist = [x for x in fruits if "a" in x]
 print(newlist)
+# Creates a list containing 5 lists, each of 8 items, all set to 0
+w, h = 8, 5
+Matrix = [[0 for x in range(w)] for y in range(h)] 
 
 # Dictionaries
 print("\nDictionaries")
